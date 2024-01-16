e945517c103060c6c98e0a23e5e2a349bd0dc5a7
requiemleaf1 <yh768@cornell.edu>
2024-01-16 15:29:32 -0500

    Updates

--- common/src/events/order-paid-event.ts
+++ common/src/events/order-paid-event.ts
@@ -1,10 +1,9 @@
 import { Subjects } from './subjects';
-
 export interface OrderPaidEvent {
-  subject: Subjects.OrderPaid;
-  data: {
-    id: string;
-    version: number;
-    
-  };
+    subject: Subjects.OrderPaid;
+    data: {
+        id: string;
+        version: number;
+        ticketId: string;
+    };
 }
